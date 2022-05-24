dc9f603d8613556994717241d8069a0a661a789f
yooonyng <yooonyng@gmail.com>
2022-05-24 11:15:14 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+## 파이썬 가상환경을 확인한다.
+## 이 앱은 구글 코랩에서 만든 아보카도 가격 예측 환경과 동일하게 하기 위해서
+## 파이썬 가상환경을 streamlit3.7로 바꿔준다.
+
+## vs code 맨 아래 오른쪽에 있는 base를 streamlit3.7로 변경한다.
